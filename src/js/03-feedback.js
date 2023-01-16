4e35e77e34e5f42db45f16b6f104c456e0aa0183
IrynaRozghon <irina.rozgon49@gmail.com>
2023-01-16 22:46:20 +0200

    2,3 task update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,33 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
 
-const formData = {};
-
-function onFormData(event) {
-  formData[event.target.name] = event.target.value;
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми.
+//  Нехай ключем для сховища буде рядок "feedback-form-state".
+const onFormData = () => {
+  const formData = { email: email.value, message: message.value };
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
-
-function onSubmitForm(event) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+};
+//Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+const onSubmitForm = event => {
   event.preventDefault();
-  event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
 
+  if (email.value && message.value) {
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+  }
+};
+// перевірка
 (function dataFromLocalStorage() {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
   if (data) {
-    email.value = data.email;
-    message.value = data.message;
+    data.email ? (email.value = data.email) : {};
+    data.message ? (message.value = data.message) : {};
   }
 })();
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
